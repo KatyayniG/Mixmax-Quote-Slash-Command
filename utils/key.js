2d7652e3a00df9814112c69c541a3d66f196665f
Katyayni Gupta <KatyayniG@users.noreply.github.com>
2016-07-24 15:30:53 -0500

    Delete key.js

--- utils/key.js
+++
@@ -1,3 +0,0 @@
-// This is my API key for the Google Drive API and is not meant to be used
-// for production traffic.
-module.exports = '721090467649-q1eo9t9b03q4k2voqc7vj8nmmbj1etgg.apps.googleusercontent.com';
